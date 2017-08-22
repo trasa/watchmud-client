0279fd06ca3c3f7dff7b455bd0e045697a8512e9
Tony Rasa <t_rasa@wargaming.net>
2017-08-22 14:58:15 -0700

    in progress

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
-# WatchMUD 
+# WatchMUD Client
 
-I started out writing something in Go and using eJabber as a backbone.
-This was fun, but XMPP / Jabber has an awful lot of overhead. So this
-is the same idea, but using web sockets instead and trying to keep
-things simple.
+A (super simple) client for the [WatchMud](http://github.com/trasa/watchmud) Project.
 
-
+The WatchMUD server hosts a static website including a LOT of Javascript 
+which worked as the original testing ground for the basic connectivity
+and some simple functionality, but things are getting out of hand. Instead
+this is a command line program that connects to WatchMUD server via WebSocket
+and from there operates much like an old telnet client... 
--- main.go
+++ main.go
@@ -1,12 +1,63 @@
 package main
 
 import (
-	"github.com/trasa/watchmud/message"
+	"github.com/gorilla/websocket"
+
 	"log"
+	"os"
+	"os/signal"
+	"time"
+	"encoding/json"
+	"github.com/trasa/watchmud/message"
 )
 
+const SERVER_ADDR = "ws://localhost:8888/ws"
+
 func main() {
-	req := message.LookRequest{}
-	resp := message.LookResponse{}
-	log.Printf("req: %s resp: %s", req, resp)
+	// TODO read from yaml configuration or something
+	// TODO override with command line args
+
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+
+	// connect to websocket
+	log.Printf("Connecting to %s", SERVER_ADDR)
+	conn, _, err := websocket.DefaultDialer.Dial(SERVER_ADDR, nil)
+	if err != nil {
+		log.Fatal("Failed to connect:", err)
+	}
+	defer conn.Close()
+	done := make(chan struct{})
+
+	go func() {
+		defer conn.Close()
+		defer close(done)
+		for {
+			_, msg, err := conn.ReadMessage()
+			if err != nil {
+				log.Println("read error: ", err)
+				return
+			}
+			log.Printf("recv: %s", msg)
+			loginResp := &message.LoginResponse{}
+			if err := json.Unmarshal(msg, loginResp); err != nil {
+				log.Printf("Unmarshall error: ", err)
+			}
+			log.Printf("loginResp %s %s", loginResp.Successful, loginResp.Player.Name)
+		}
+	}()
+
+	// send login request
+	playerName := "somedood"
+	password := "NotImplemented"
+	loginReq := make(map[string]string)
+	loginReq["msg_type"] = "login"
+	loginReq["player_name"] = playerName
+	loginReq["password"] = password
+
+	j, _ := json.Marshal(loginReq)
+	log.Printf("sending \n%s\n", j)
+
+	conn.WriteJSON(loginReq)
+	time.Sleep(time.Second * 10)
 }
