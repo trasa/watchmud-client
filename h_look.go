cdfdc841f14441ac386def573a2f0493b698b7d0
Tony Rasa <t_rasa@wargaming.net>
2017-09-06 17:29:03 -0700

    translating looks works now

--- h_look.go
+++ h_look.go
@@ -2,25 +2,26 @@ package main
 
 import (
 	"fmt"
-	"github.com/trasa/watchmud/message"
 	"github.com/trasa/watchmud/direction"
+	"github.com/trasa/watchmud/message"
 )
 
 func (c *Client) handleLookResponse(resp *message.LookResponse) {
 	if !resp.IsSuccessful() {
 		fmt.Println("Error Looking:", resp.GetResultCode())
 	} else {
-		fmt.Println(resp.Name)
-		fmt.Println("\n", resp.Description)
+		fmt.Println(resp.RoomDescription.Name)
+		fmt.Println()
+		fmt.Println(resp.RoomDescription.Description)
 		// other players
-		if len(resp.Players) > 0 {
+		if len(resp.RoomDescription.Players) > 0 {
 			fmt.Println("Other Players:")
-			for _, p := range resp.Players {
+			for _, p := range resp.RoomDescription.Players {
 				fmt.Println(p)
 			}
 		}
 		// obvious exits
-		if exits, err := direction.ExitsToString(resp.Exits); err == nil {
+		if exits, err := direction.ExitsToString(resp.RoomDescription.Exits); err == nil {
 			fmt.Println("Obvious Exits:", exits)
 		} else {
 			fmt.Println("Error Getting exits:", err)
