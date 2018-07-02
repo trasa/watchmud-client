18c03f734b27f9cbd4318ab90811db50174bd12f
Tony Rasa <trasa@meancat.com>
2018-07-02 13:55:35 -0700

    kill handler

--- h_kill.go
+++ h_kill.go
@@ -6,6 +6,21 @@ import (
 )
 
 func (c *Client) handleKillResponse(r *message.KillResponse) {
-	// TODO
-	fmt.Println("Response code should go here.")
+	if r.Success {
+		fmt.Println("Ok.")
+		return
+	}
+	switch r.GetResultCode() {
+	case "ALREADY_FIGHTING":
+		fmt.Println("You're already in a fight!")
+	case "TARGET_NOT_FOUND":
+		fmt.Println("You don't see them here.")
+	case "NO_FIGHT_ROOM":
+		fmt.Println("You feel a sense of peace.")
+	case "NO_FIGHT":
+		fmt.Println("Something about them convinces you not to fight.")
+	default:
+		c.printError(r, r.GetResultCode())
+	}
+
 }
